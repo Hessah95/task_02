9b19371052c61112c2f44effc7fe75b311df35a4
unknown <hessah.alhamadi.95@gmail.com>
2019-08-20 12:11:20 +0300

    Hessah ALHamadi & Samira Hesham

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def vw (request) :
+
+	context = {
+	"msg" : "Hello world!",
+	}
+
+	return render (request, 'test.html', context)
